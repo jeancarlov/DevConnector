56f19e2527254a2a6acc0e952d0af3bea93faeec
Jean Valderrama <jeanvalderrama@jeans-mbp.home>
2019-06-02 22:46:15 -0400

    register and login page and links setup before redux

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,13 +1,61 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>
-      <h1>
-      Login
-      </h1>
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+   
+    email: "",
+    password: "",
+    
+  });
 
-export default Login
+  // pull name and email out by using destructure
+  const {  email, password} = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log("Sucesss");
+    
+  };
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign in</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sing into your account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+       
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+          />
+          
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
